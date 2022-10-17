6985539f2c09b89046182b88b1882db5b2b5ab1a
Dimitris Raviolos <dimitris.raviolos@openbet.com>
2022-10-17 20:01:51 +0300

    Added headings with group identifiers. Closes #15

--- src/EventList.js
+++ src/EventList.js
@@ -1,6 +1,15 @@
 import * as React from 'react';
-import { Datagrid, DatagridBody, RecordContextProvider } from 'react-admin';
 import { TableCell, TableRow } from '@mui/material';
+import {
+  Datagrid,
+  DatagridBody,
+  RecordContextProvider,
+  useGetList,
+  useList,
+  ListContextProvider,
+} from 'react-admin';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
 
 const MyDatagridRow = ({ record, id, onToggleItem, children, selected, selectable }) => (
   <RecordContextProvider value={record}>
@@ -17,4 +26,48 @@ const MyDatagridRow = ({ record, id, onToggleItem, children, selected, selectabl
 const MyDatagridBody = props => <DatagridBody {...props} row={<MyDatagridRow />} />;
 const MyDatagrid = props => <Datagrid {...props} body={<MyDatagridBody />} />;
 
-export default MyDatagrid;
+const MyContext = props => {
+  const { children, isLoading, data, group } = props;
+  const contextProvider = useList({ data, isLoading });
+
+  return (
+    <ListContextProvider value={contextProvider}>
+      <Card style={{ marginBottom: 10 }}>
+        <CardHeader title={`Group ${String.fromCharCode(group+65)}`} />
+        <MyDatagrid rowClick="edit" header={() => {}}>
+          {children}
+        </MyDatagrid>
+      </Card>
+    </ListContextProvider>
+  )
+}
+
+const MyCustomList = props => {
+  const { children } = props;
+  const { data, isLoading } = useGetList('events', {
+    pagination: { page: 1, perPage: 10 },
+    sort: { field: 'id', order: 'DESC' },
+  });
+  const tofilter = data || [];
+
+  const fromfilter = tofilter.reduce((acc, cur) => {
+    const i = cur.group.charCodeAt(0)-65;
+    if (acc[i] === undefined) {
+      acc[i] = [];
+    }
+    acc[i].push(cur);
+    return acc;
+  }, []);
+
+  return (
+    <React.Fragment>
+      {fromfilter.map((d, i) => (
+        <MyContext key={i} data={d} isLoading={isLoading} group={i}>
+          {children}
+        </MyContext>
+      ))}
+    </React.Fragment>
+  );
+};
+
+export default MyCustomList;

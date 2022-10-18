8bf0a0ad2cf0e6c9ddbd0a4a7df0d59461c40530
Dimitris Raviolos <theio.vrefos@gmail.com>
2022-10-18 13:47:16 +0300

    added the price on the event list

--- src/components/CountryItem.js
+++ src/components/CountryItem.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
+// import CardActions from '@mui/material/CardActions';
 import Avatar from '@mui/material/Avatar';
 import { CircleFlag } from 'react-circle-flags'
 import BalanceIcon from '@mui/icons-material/Balance';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import PlaceBetButton from './PlaceBetButton';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Button from '@mui/material/Button';
 
 const CountryItem = props => {
-  const { id, country_code, name, subheader, parent_id } = props;
+  const { id, country_code, name, subheader, parent_record } = props;
+  const { id: parent_id } = parent_record;
+
+  let price;
+  if (subheader === "home") {
+    price = parent_record.home_price;
+  } else if (subheader === "away") {
+    price = parent_record.away_price;
+  } else {
+    price = parent_record.draw_price;
+  }
 
   return (
     <Card>
@@ -30,12 +43,23 @@ const CountryItem = props => {
         subheader={subheader}
         action={
           <Stack direction="row" spacing={1} sx={{ padding: 1 }}>
-            <PlaceBetButton stake={1} id={id} event_id={parent_id} />
-            <PlaceBetButton stake={2} id={id} event_id={parent_id} />
-            <PlaceBetButton stake={5} id={id} event_id={parent_id} />
+                          <Button color="primary" variant="contained">{price.toFixed(2)}</Button>
+            <ButtonGroup variant="contained">
+
+              <PlaceBetButton stake={1} id={id} event_id={parent_id} />
+              <PlaceBetButton stake={2} id={id} event_id={parent_id} />
+              <PlaceBetButton stake={5} id={id} event_id={parent_id} />
+            </ButtonGroup>
           </Stack>
         }
       />
+      {/* <CardActions>
+        <Stack direction="column" spacing={1} sx={{ padding: 1 }}>
+          <PlaceBetButton stake={1} id={id} event_id={parent_id} />
+          <PlaceBetButton stake={2} id={id} event_id={parent_id} />
+          <PlaceBetButton stake={5} id={id} event_id={parent_id} />
+        </Stack>
+      </CardActions> */}
     </Card>
   );
 }

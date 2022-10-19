8f454f9c7d376c2b35fd94115479d125ede73eae
Dimitris Raviolos <theio.vrefos@gmail.com>
2022-10-19 11:37:04 +0300

    cleanup

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './reducers/root';
 
-// store.dispatch(user.)
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
